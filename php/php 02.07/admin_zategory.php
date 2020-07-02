4cc61321883bd1e247562f35c334ab24da3341d6
redy2219 <59687967+redy2219@users.noreply.github.com>
2020-07-02 20:01:28 +0300

    Create admin_zategory.php

---
+++ php/php 02.07/admin_zategory.php
@@ -0,0 +1,85 @@
+<?php require_once "./lib/functions.php";
+
+$mysql = connectDatabase();
+if (!empty($_FILES['uploadImage']['name'])) {
+    $uploaddir = 'images/';
+    $imagePath = $uploaddir . basename($_FILES['uploadImage']['name']);
+    move_uploaded_file($_FILES['uploadImage']['tmp_name'], $_SERVER['DOCUMENT_ROOT'] . '/' . $imagePath);
+
+    if (empty($_GET['galleryImage'])) {
+        $galleryImage = basename($_FILES['uploadImage']['name']);
+    } else {
+        $galleryImage = $_GET['galleryImage'];
+    }
+    $imageUrlPath = "./$imagePath";
+    $sql = "INSERT galleryImages(galleryName, galleryPath) VALUES('$galleryImage','$imageUrlPath')";
+    $mysql->query($sql);
+    header('Location: http://gallery.docker.oblako-1c.ru/admin.php');
+    exit();
+}
+
+
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/gallery.css">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+            crossorigin="anonymous"></script>
+    <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>gallary</title>
+</head>
+<body>
+
+<div class="container-fluid">
+    <div class="row">
+
+        <div class="col-8">
+            <h2>Images</h2>
+            <table class="table" id="dynamic">
+                <thead class="thead-light">
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Path</th>
+                    <th scope="col">Delete</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                if (!empty($_GET['category_id'])) {
+                    $categoryId = $_GET['category_id'];
+                    $query = "SELECT *  FROM galleryCategoryImages
+         LEFT JOIN galleryImages ON galleryCategoryImages.id_images = galleryImages.id
+          WHERE galleryCategoryImages.id_category=" . $categoryId . " ";
+                    $result = $mysql->query($query);
+                    while ($sql = $result->fetch_object()) {
+                        echo("<tr>");
+                        echo("<td>$sql->id_images<?td>");
+                        echo("<td>$sql->galleryName<?td>");
+                        echo("<td> $sql->galleryPath<?td> ");
+                        echo("</tr>");
+                    }
+                } ?>
+                </tbody>
+            </table>
+            <form method="post" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="galleryImage"><b>Upload image</b></label>
+                    <input type="file" name="uploadImage"/>
+                </div>
+                <div class="form-group">
+                    <label for="galleryImage">Image name</label>
+                    <input type="text" class="form-control" name="galleryImage" id="galleryImage"
+                           placeholder="Image name">
+                </div>
+                <button type="submit" class="btn btn-secondary">Submit</button>
+            </form>
+</body>
